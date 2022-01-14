cc54da8adb45ba99b0fe16c19518cbc74ed7a5b1
Vasyl M <makohin.v@gmail.com>
2022-01-15 01:14:02 +0200

    feat: add function reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = reverse = (n) => {
+    const str = Math.abs(n).toString();
+    const strReverse = str.split("").reverse().join("");
+    return strReverse;
+};
